63326dcd552724c4a7631504de3716349d3eea4d
Ayşe <aysegoktas727@gmail.com>
2024-06-21 13:55:44 +0300

    CarAdd

--- CarRental/CarRental/Models/Functions.cs
+++ CarRental/CarRental/Models/Functions.cs
@@ -19,7 +19,7 @@ namespace CarRental.Models
 
         // Constructor to initialize the connection string and setup the SqlConnection and SqlCommand
         public Functions()
-        { 
+        {
             _connectionStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ASUS\Documents\OnlineCarRental.mdf;Integrated Security=True;Connect Timeout=30;"; // Initialize the connection string (should be replaced with actual connection string)
             _connection = new SqlConnection(_connectionStr); // Initialize the SqlConnection with the connection string
             _command = new SqlCommand(); // Initialize the SqlCommand object
@@ -37,6 +37,7 @@ namespace CarRental.Models
         }
 
         // Method to execute an INSERT, UPDATE, or DELETE query and return the number of affected rows
+        /*
         public int SetData (string Query)
         {
             int rent = 0;// Variable to hold the number of affected rows
@@ -53,5 +54,27 @@ namespace CarRental.Models
 
             return rent; // Return the number of affected rows
         }
+        */
+        public int SetData(string query, Dictionary<string, object> parameters)
+        {
+            int affectedRows = 0;
+
+            using (SqlConnection connection = new SqlConnection(_connectionStr))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    foreach (var parameter in parameters)
+                    {
+                        command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                    }
+
+                    connection.Open();
+                    affectedRows = command.ExecuteNonQuery();
+                }
+            }
+
+            return affectedRows;
+        }
+
     }
 }
--- CarRental/CarRental/View/Admin/Cars.aspx.cs
+++ CarRental/CarRental/View/Admin/Cars.aspx.cs
@@ -20,21 +20,54 @@ namespace CarRental.View.Admin
         {
             try
             {
-                if (cLicenceNumber.Value == "" || cBrand.Value =="" || cModel.Value == "" || cPrice.Value == "" || cColor.Value == "" )
+                if (string.IsNullOrEmpty(cLicenceNumber.Value) ||
+                    string.IsNullOrEmpty(cBrand.Value) ||
+                    string.IsNullOrEmpty(cModel.Value) ||
+                    string.IsNullOrEmpty(cPrice.Value) ||
+                    string.IsNullOrEmpty(cColor.Value))
                 {
                     ErrorMsg.InnerText = "FILL THE INFORMATION!";
-                } 
+                }
                 else 
                 {
+                    
                     string CarLicenceNumber = cLicenceNumber.Value;
                     string CarBrand = cBrand.Value;
                     string CarModel = cModel.Value;
                     int CarPrice = Convert.ToInt32(cPrice.Value.ToString());
                     string CarColor = cColor.Value;
                     string CarStatus = carAvailibity.SelectedValue;
-                    string CarQuery = "insert into tblCar values {0}, {1}, {2}, {3}, {4}, {5}";
-                    CarQuery = String.Format(CarLicenceNumber, CarBrand, CarModel, CarPrice, CarColor, CarStatus);
-                    _connection.SetData(CarQuery);
+                    //string CarQuery = "insert into tblCar values {0}, {1}, {2}, {3}, {4}, {5}";
+                    //CarQuery = String.Format(CarLicenceNumber, CarBrand, CarModel, CarPrice, CarColor, CarStatus);
+                    //_connection.SetData(CarQuery);
+
+                    //ErrorMsg.InnerText = "CAR IS ADDED!";
+
+
+                    /*
+                    string CarLicenceNumber = cLicenceNumber.Value;
+                    string CarBrand = cBrand.Value;
+                    string CarModel = cModel.Value;
+                    int CarPrice = Convert.ToInt32(cPrice.Value);
+                    string CarColor = cColor.Value;
+                    string CarStatus = carAvailibity.SelectedValue;
+                    */
+                    // Parametreli sorgu oluşturma
+                    string CarQuery = "INSERT INTO tblCar (carPlateNumber, carBrand, carModel, carPrice, carColor, carStatus) " +
+                                      "VALUES (@CarLicenceNumber, @CarBrand, @CarModel, @CarPrice, @CarColor, @CarStatus)";
+
+                    // Parametreleri ekleme
+                    var parameters = new Dictionary<string, object>
+                    {
+                        { "@CarLicenceNumber", CarLicenceNumber },
+                        { "@CarBrand", CarBrand },
+                        { "@CarModel", CarModel },
+                        { "@CarPrice", CarPrice },
+                        { "@CarColor", CarColor },
+                        { "@CarStatus", CarStatus }
+                    };
+
+                    _connection.SetData(CarQuery, parameters);
 
                     ErrorMsg.InnerText = "CAR IS ADDED!";
                 }
